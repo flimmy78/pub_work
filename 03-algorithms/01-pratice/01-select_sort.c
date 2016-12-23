726643b70bc79cab0b7b8250950631f36572b62e
fly <1358326274@qq.com>
2016-12-23 18:10:59 +0800

    changed 2016/12/23-18:11

--- 03-algorithms/01-pratice/01-select_sort.c
+++ 03-algorithms/01-pratice/01-select_sort.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define N 8
+#define DEBUG_TEST
 
 void select_sort(int a[], int n)
 {
 	int i,j;
-	for(i = 0; i < n-1; i++)	//进行N-1轮选择
+
+	for(i = 0; i < n-1; i++)	//进行n-1轮选择
 	{
-		int min_index = i;	//假定最小数的下标为i
-		for(j = i+1; j<n; j++)
+		int min_index = i;
+		for(j = i+1; j < n; j++)	//选出未排序的最小的数
 		{
 			if(a[j] < a[min_index])
 			{
 				min_index = j;
 			}
 		}
+#ifdef DEBUG_TEST
+		printf("第%d轮选择：a[min_index] = %d\ta[i] = %d\n",(i+1),a[min_index],a[i]);
+#endif
 
-		if(i != min_index)
+		if(i != min_index)	//将第n小的数排在相应的位置
 		{
-			int tmp = a[i];
+			int tmp;
+			tmp = a[i];
 			a[i] = a[min_index];
 			a[min_index] = tmp;
+#ifdef DEBUG_TEST
+			printf("    交换后：a[min_index] = %d\ta[i] = %d\n",a[min_index],a[i]);
+#endif
 		}
 	}
 }
 
+#ifdef DEBUG_TEST
 int main()
 {
-	int num[N] = {89, 38, 11, 78, 96, 44, 19, 25};
-
-	select_sort(num, N);
-
+	int a[N]= {89, 100, 12, 56, 4, 789, 55, 456};
 	int i;
-	for(i = 0; i<N ;i++)
-		printf("%d  ",num[i]);
-	printf("\n");
+
+	puts("排序前：");
+	for(i = 0; i<N; i++)
+		printf("%d  ",a[i]);
+	puts("");
+
+	select_sort(a, N);
+	puts("排序后：");
+	for(i = 0; i<N; i++)
+		printf("%d  ",a[i]);
+	puts("");
 
 	return 0;
 }
+#endif
---
+++ 03-algorithms/01-pratice/02-bubble_sort.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define N 8
+#define DEBUG_TEST
+//#define DEBUG_TEST_
+
+void bubble_sort(int a[], int n);
+void bubble_sort_better(int a[], int n);
+void printf_arr(int a[], int n);
+
+#ifdef DEBUG_TEST
+int main(void)
+{
+	int a[N] = {99, 55, 12, 45, 23, 11, 77, 88};
+	int i;
+
+	puts("排序前");
+#if (0)
+	for(i = 0; i < N; i++)
+	{
+		printf("%d  ",a[i]);
+	}
+	puts("");
+#else
+	printf_arr(a,N);
+#endif
+	
+#if 0
+	bubble_sort(a,N);
+#else
+	bubble_sort_better(a,N);
+#endif
+
+	puts("排序后");
+	printf_arr(a,N);
+
+	return 0;
+}
+#endif
+
+/* 冒泡排序法 */
+void bubble_sort(int a[], int n)
+{
+	int i, j;
+
+	for(i = 0; i< n-1; i++)	//进行n-1轮比较
+	{
+		printf("\t第%d轮排序: %d\n",(i+1),a[0]);
+		for(j = 0; j < n-1-i; j++)	//与n-1-i个数进行比较
+		{
+			if(a[j] > a[j+1])		//比较大的数往后移动
+			{
+#ifdef DEBUG_TEST_
+				printf("交换前：a[%d] = %d\ta[%d] = %d\n",j,a[j],j+1,a[j+1]);
+#endif
+				int tmp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = tmp;
+#ifdef DEBUG_TEST_
+				printf("\t交换后：a[%d] = %d\ta[%d] = %d\n",j,a[j],j+1,a[j+1]);
+#endif
+			}
+#if (1)
+			printf_arr(a,n);
+#endif
+		}
+#ifdef DEBUG_TEST_
+		printf("第%d轮排序后:",(i+1));
+		printf_arr(a,n);
+#endif
+	}
+}
+
+/* 冒泡排序法（改进版） */
+void bubble_sort_better(int a[], int n)
+{
+	int i, j;
+
+	for(i = 0; i<(n-1); i++)	//进行n-1轮排序
+	{
+		printf("\t第%d轮排序: %d\n",(i+1),a[0]);
+		bool isSorted = true;
+		for(j = 0; j<(n-1-i); j++)
+		{
+			if(a[j] > a[j+1])
+			{
+				int tmp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = tmp;
+				isSorted = false;
+			}
+			printf_arr(a,N);
+		}
+
+		/* */
+		if(isSorted)
+		{
+			break;
+		}
+	}
+
+}
+
+/* 打印数组 */
+void printf_arr(int a[], int n)
+{
+	int i;
+	for(i = 0; i<N ; i++)
+	{
+		printf("%d  ",a[i]);
+	}
+	puts("");
+}
