fe8f30d03ee3a47897dd6b3b68d7da9000f18f6c
Fly <1358326274@qq.com>
2017-02-23 09:44:57 +0800

    20170223-morning work

---
+++ 11-Proj/rookie_try/C++/01-hello.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************
+ *   > File Name: 01-hello.cpp
+ *   > Author: fly
+ *   > Mail: XXXXXXXX@icode.com
+ *   > Create Time: Thu 23 Feb 2017 09:11:59 AM CST
+ ******************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello, the world of C++ !" << endl;//可以用“\n”代替“endl”
+
+    return 0;
+}
---
+++ 11-Proj/rookie_try/JAVA/code_01/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ *   > File Name: HelloWorld.java
+ *   > Author: fly
+ *   > Mail: XXXXXXXX@icode.com
+ *   > Create Time: Thu 23 Feb 2017 09:24:42 AM CST
+ */
+public class HelloWorld{
+    public static void main(String []args){
+        System.out.println("Hello ,the world of java");
+    }
+}
