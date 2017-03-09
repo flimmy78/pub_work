026d30035d5b71224bd601cc74b292574b8ab52c
fly <1358326274@qq.com>
2017-03-09 23:38:22 +0800

    homework

--- 11-Proj/fly_work/02-EmbeddedApplicationDesign/01-stdIO/README.c
+++ 11-Proj/fly_work/02-EmbeddedApplicationDesign/01-stdIO/README.c
@@ -1,24 +1,17 @@
 1.1-Linux系统调用和用户编程接口
-1.1.1-系统调用：
-1.进程控制
-2.进程间通讯
-3.文件系统控制
-4.存储管理
-5.网络管理
-6.套接字管理
-7.用户管理
+1.1.1-系统调用：1.进程控制;2.进程间通讯;3.文件系统控制;4.存储管理;5.网络管理;6.套接字管理;7.用户管理;
 
 1.1.2用户编程接口
+（1）系统调用接口功能非常简单，无法满足程序的需求；
+（2）不同操作系统的系统调用接口不兼容，程序移植时工作量非常巨大；
 
 1.2-Linux标准I/O概述
-1.2.1-标准I/O的由来
-1.2.2-流的含义
-1.全缓冲
-2.行缓冲
-3.无缓冲
+1.2.1-标准I/O的由来：ANSI C中定义的用于I/O操作的一系列函数；
+1.2.2-流的含义--->流的缓冲类型：1.全缓冲；2.行缓冲；3.无缓冲；
 
 1.3-标准I/O编程
 1.3.1-流的打开
+标准流：1.标准输入流stdin、标准输出流stdout和标准错误流stderr
 #include <stdio.h>
 
 FILE *fopen(const char *path, const char *mode);
@@ -26,6 +19,16 @@ FILE *fopen(const char *path, const char *mode);
 FILE *fdopen(int fd, const char *mode);
 
 FILE *freopen(const char *path, const char *mode, FILE *stream);
+    mode的取值说明：
+    r或者rb：
+    r+或者r+b：
+            以上文件必须存在；
+    w或者wb：
+    w+或者w+b：
+            存在擦除，不存在创建；
+    a或者ab：
+    a+或者a+b：
+            以附加方式打开文件，不存在则创建；
 
 1.3.2-流的关闭
 int fclose(FILE* stream);
