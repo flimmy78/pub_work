a9ea68819d3ad8f423b8e1f0478fd4bac80d20b0
Fly <xxxxxxxx@git.com>
2017-03-31 18:02:25 +0800

    20170331-18:02 7G

--- 11-Proj/rookie_try/LUA/DataType.lua
+++ 11-Proj/rookie_try/LUA/DataType.lua
@@ -9,12 +9,3 @@ print(type(nil))                --> nil
 print(type(type(X)))            --> string
 
 
-tab1 = { key1 = "val1", key2 = "val2", "val3" }
-for k, v in pairs(tab1) do
-    print(k .. " - " .. v)
-end
-     
-tab1.key1 = nil
-for k, v in pairs(tab1) do
-    print(k .. " - " .. v)
-end
