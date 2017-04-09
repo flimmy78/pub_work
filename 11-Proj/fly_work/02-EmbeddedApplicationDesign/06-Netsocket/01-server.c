fe04681162caa6229c92153083a413320bda3a90
Fly <xxxxxxxx@git.com>
2017-04-09 23:18:08 +0800

    20170409-23:18

--- 11-Proj/fly_work/02-EmbeddedApplicationDesign/06-Netsocket/01-server.c
+++ 11-Proj/fly_work/02-EmbeddedApplicationDesign/06-Netsocket/01-server.c
@@ -2,7 +2,7 @@
  *   > File Name: 01-server.c
  *   > Author: fly
  *   > Mail: XXXXXXXX@icode.com
- *   > Create Time: Sun 15 Jan 2017 10:39:41 PM CST
+ *   > Create Time: Sun 09 Apr 2017 10:46:56 PM CST
  ******************************************************************/
 
 #include <stdio.h>
@@ -14,7 +14,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define BUFFER_SIZE 128
+#define BUFFER_SIZE     128
 
 int main(int argc, char* argv[])
 {
@@ -23,8 +23,8 @@ int main(int argc, char* argv[])
     socklen_t peerlen;
     char buf[BUFFER_SIZE];
 
-    if(argc < 3){
-        printf("Usage :%s <ip> <port>\n",argv[0]);exit(-1);
+    if(argc  < 3){
+        printf("Usage : %s <ip> <port> \n",argv[0]);exit(-1);
     }
 
     /*建立socket连接*/
@@ -34,33 +34,43 @@ int main(int argc, char* argv[])
 
     printf("listenfd = %d\n",listenfd);
 
-    /*设置sockaddr_in结构体*/
-    bzero(&servaddr,sizeof(servaddr));
+    /*设置sockaddr_in结构体相关参数*/
+    bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(atoi(argv[2]));
     servaddr.sin_addr.s_addr = inet_addr(argv[1]);
 
-    /*绑定函数bind()*/
-    if(bind(lisenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
+    /*绑定函数bind（）*/
+    if(bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
         perror("bind");exit(-1);
     }
-
     printf("bind success!\n");
 
-    /*调用listen()函数，设置监听模式*/
+    /*调用listen（）函数，设置监听模式*/
     if(listen(listenfd, 10) == -1){
         perror("listen");exit(-1);
     }
-
     printf("Listening...\n");
 
-    /*调用accept()函数，等待客户端的连接*/
+    /*调用accept（）函数，等待客户端的连接*/
     peerlen = sizeof(cliaddr);
     while(1){
-        if((connfd = accept()) < 0)
+        if((connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &peerlen)) < 0){
+            perror("accept");exit(-1);
+        }
+
+        /*调用recv（）函数接收客户端发送的数据*/
+        memset(buf, 0, sizeof(buf));
+        if(recv(connfd, buf, BUFFER_SIZE, 0) == -1){
+            perror("recv");exit(-1);
+        }
+        printf("Received a message :%s\n", buf);
+
+        strcpy(buf, "Welcome to server");
+        send(connfd, buf, BUFFER_SIZE, 0);
+        close(connfd);
     }
 
-
-
+    close(listenfd);
     return 0;
 }
