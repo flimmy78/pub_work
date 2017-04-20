e5c1a183853b8db7bc072c8bc33f6dd4ddcfca43
Fly <xxxxxxxx@git.com>
2017-04-20 23:18:37 +0800

    23:18Home

---
+++ 11-Proj/fly_work/02-EmbeddedApplicationDesign/07-SuperNet/README.c
@@ -0,0 +1,7 @@
+7.1.1-套接字超时检测
+    套接字的常用选项及相关说明
+    选项名称        说明        数据类型
+    LEVEL:SOL_SOCKET
+    SO_BROADCAST        允许发送广播数据        int
+    SO_DEBUG            允许调试                int
+    SO_DONTROUTE        
